2a2f81fb51f39be33cf5b6979aae94fbeb444026
jaime perdue <jperdue@ucsd.edu>
2018-01-25 10:18:56 -0800

    did some cool stuff

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,11 +10,13 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$(".jumbotron h1").text("when I was a young warthogggggg (when he was a young warthogggg)");
+		$("#testjs").text("Please wait...");
+		$("#testjs").toggleClass("active");
 	});
-
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
 }
 
 function projectClick(e) { 
@@ -22,10 +24,14 @@ function projectClick(e) { 
     e.preventDefault();
     // In an event handler, $(this) refers to      
     // the object that triggered the event      
-    $(this).css("background-color", "#7fff00");
-
-	var containingProject = $(this).closest(".project");
-    	containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+    //$(this).css("background-color", "#7fff00");
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.html("<p>DON'T CLICK- NOOOOOOO</p>");
+       $(containingProject).fadeOut();
+    }
 }
 
-}
